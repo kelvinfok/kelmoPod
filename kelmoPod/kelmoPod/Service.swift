01b949abbf1c876d71b92601f3d3d8239986e888
kelvinfok <punkd1@gmail.com>
2018-08-31 14:39:39 +0800

    finished framework

---
+++ kelmoPod/kelmoPod/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  kelmoPod
+//
+//  Created by Kelvin Fok on 31/8/18.
+//  Copyright © 2018 kelvinfok. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "This is a random string"
+    }
+    
+}
---
+++ kelmoPod/kelmoPod/kelmoPod.h
@@ -0,0 +1,19 @@
+//
+//  kelmoPod.h
+//  kelmoPod
+//
+//  Created by Kelvin Fok on 31/8/18.
+//  Copyright © 2018 kelvinfok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kelmoPod.
+FOUNDATION_EXPORT double kelmoPodVersionNumber;
+
+//! Project version string for kelmoPod.
+FOUNDATION_EXPORT const unsigned char kelmoPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kelmoPod/PublicHeader.h>
+
+
